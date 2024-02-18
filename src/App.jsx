db3eeb717f82158442c38eb87e9517b9cc35aaa9
TonyTech-GIT <toomgbebu@gmail.com>
2024-02-18 16:30:01 +0100

    first commit on e-commerce product project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Product from "./components/Product"
+
+
+const App = () => {
+
+  return (
+    <section className='home'>
+      <Header />
+
+      <Product />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+
+const Product = () => {
+    return (
+        <div>
+            <h1>This is the product section</h1>
+        </div>
+    )
+}
+
+export default Product
